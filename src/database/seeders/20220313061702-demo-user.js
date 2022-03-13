2c254b39ff56e0a7743d6055471a63cfadea400e
maria.araujosza <maria.araujosza@gmail.com>
2022-03-13 02:40:49 -0400

    feat: user admin

---
+++ src/database/seeders/20220313061702-demo-user.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [{
+      email: 'admin@admin.com',
+      senha: 'admin',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
