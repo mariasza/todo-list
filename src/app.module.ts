593367a6e1c58d68f1165fdc80b96d217bb2f979
maria.araujosza <maria.araujosza@gmail.com>
2022-03-13 02:37:43 -0400

    feat: sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
-import { PaginationService } from './utils/pagination.service';
+
+import { SequelizeModule } from '@nestjs/sequelize';
+
 import { TodoModule } from './app/todo/todo.module';
 import { UserModule } from './app/user/user.module';
 
+import { JwtStrategy } from './utils/auth/jwt.strategy';
+import { JwtAuthGuard } from './utils/auth/jwt-auth.guard';
+import { APP_GUARD } from '@nestjs/core';
+
+import { PaginationService } from './utils/pagination/pagination.service';
+import { User } from './database/models/user.model';
+import { Todo } from './database/models/todo.model';
+
 @Module({
-  imports: [TodoModule, UserModule],
-  providers: [PaginationService],
+  imports: [SequelizeModule.forRoot({
+    dialect: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'user',
+    password: '12345',
+    database: 'mysqldb',
+    models: [User, Todo],
+  }),
+    TodoModule, UserModule],
+  providers: [
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    PaginationService],
 })
-export class AppModule {}
+export class AppModule { }
