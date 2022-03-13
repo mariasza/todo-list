f7b7fb3fc962678c0a9cc4b9e5b2cf8fbfa0eddc
maria.araujosza <maria.araujosza@gmail.com>
2022-03-13 02:38:19 -0400

    feat: config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('TODO List')
+    .setDescription('API for to-do list control')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
