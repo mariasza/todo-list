aa87a886578cfe44942a43d0701076be7de5e182
maria.araujosza <maria.araujosza@gmail.com>
2022-03-13 02:38:01 -0400

    feat: config sequelize

---
+++ src/database/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    development: {
+        username: 'user',
+        password: '12345',
+        database: 'mysqldb',
+        host: 'localhost',
+        port: 3306,
+        dialect: 'mysql',
+
+    }
+};
