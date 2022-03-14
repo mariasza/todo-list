c1e48c53eb8d5616066c14f96830b490924ded06
maria.araujosza <maria.araujosza@gmail.com>
2022-03-13 20:29:26 -0400

    feat: encriptacçao na senha do admin

--- src/database/seeders/20220313061702-demo-user.js
+++ src/database/seeders/20220313061702-demo-user.js
@@ -4,7 +4,7 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Users', [{
       email: 'admin@admin.com',
-      password: 'admin',
+      password: '$2a$08$VCljTHtrTcOlA3iwX7o7oOW.Nvuwu9NC299mOlOuCpVopgWmIEXb2',
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
